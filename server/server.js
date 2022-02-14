7ddf4828d072ed1bdeda589d97e14c1e8b4e4f3a
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-14 16:04:40 -0600

    Reconfigured server and pool for heroku

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use('/list', listRouter);
 
 
 //< ------------------ PORT LISTENER -------------------->
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
     
